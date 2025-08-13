5bad80eec93c1c267ac59446510563db117f9863
prajwal22 <prajwalchoudhari22@gmail.com>
2025-08-13 13:03:44 +0530

    added bankaccount

---
+++ c++/BankAccount.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount
+{
+
+protected:
+    int accountNumber;
+    string holderName;
+    double balance;
+
+public:
+    BankAccount()
+    {
+        cout << "default const for banaccount" << endl;
+    }
+
+    BankAccount(int accNo, string name, double bal)
+    {
+        accountNumber = accNo;
+        holderName = name;
+        balance = bal;
+    }
+
+    double deposit(double amount)
+    {
+        cout << "needs to return the amount after the deposit" << endl;
+        balance = balance + amount;
+        return balance;
+    }
+
+    double withdraw(double amount)
+    {
+
+        cout << "needs to display the amount after withdraw" << endl;
+        balance = balance - amount;
+        return balance;
+    }
+};
+
+class SavingsAccount : public BankAccount
+{
+private:
+    double interestRate;
+    double minimumBalance;
+    int maturityPeriod;
+
+public:
+    SavingsAccount()
+    {
+        cout << "default constructor for savingsacc" << endl;
+    }
+
+    SavingsAccount(double inte_rate, double min_bal, int matu_per, int accNo, string name, double bal) : BankAccount(accNo, name, bal)
+    {
+        cout << "parameterised const for savingsac" << endl;
+        interestRate = inte_rate;
+        minimumBalance = min_bal;
+        maturityPeriod = matu_per;
+    }
+
+    int calculateInterest(double inte_rate, double min_bal, int matu_period, int interest)
+    {
+        cout << "interest is calculated here" << endl;
+        interest = (min_bal * inte_rate * matu_period) / 100;
+        return interest;
+    }
+
+    void renewAccount()
+    {
+        cout << "account renewal after maturity" << endl;
+        cout << "your account is due for renewal" << endl;
+    }
+};
+
+class CurrentAccount : public BankAccount
+{
+private:
+    double overdraftLimit;
+    double transactionFee;
+    string businessType;
+
+    CurrentAccount()
+    {
+        cout << "default constructor for curracc" << endl;
+    }
+
+    CurrentAccount(double od_limit, double trans_fee, string bus_type, int accNo, string name, double bal) : BankAccount(accNo, name, bal)
+    {
+        overdraftLimit = od_limit;
+        transactionFee = trans_fee;
+        businessType = bus_type;
+    }
+
+public:
+    int processBusinessTransaction(double amount, double transaction_fee)
+    {
+        if (amount + transaction_fee <= balance + overdraftLimit)
+        {
+            balance = balance - (amount + transactionFee);
+            cout << "Transaction Successful!" << endl;
+        }
+        else
+        {
+            cout << "Transaction Failed!" << endl;
+        }
+        // status
+    }
+
+    double getOverdraftInfo(double overdraftLimit)
+    {
+        return overdraftLimit;
+    }
+};
+
+int main()
+{
+}
--- c++/hybrid.cpp
+++ c++/hybrid.cpp
@@ -1,8 +1,62 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+class Base
+{
+private:
+    int privateVar;
 
+protected:
+    int protectedVar;
 
+public:
+    int publicVar;
 
-int main(){
+    Base()
+    {
+        cout << "Base constructor called" << endl;
+    }
+};
 
+// Public visibility mode
+class PublicChild : public Base
+{
+    PublicChild()
+    {
+        cout << "PublicChild constructor called" << endl;
+        publicVar = 10;
+        protectedVar = 10;
+        // privateVar = 10;
+        cout << publicVar << protectedVar << endl;
+    }
+};
+
+class ProtectedChild : protected Base
+{
+    ProtectedChild()
+    {
+        cout << "PublicChild constructor called" << endl;
+        publicVar = 11;
+        protectedVar = 11;
+        // privateVar = 11;
+        cout << publicVar << protectedVar << endl;
+    }
+};
+
+class PrivateChild : private Base
+{
+    PrivateChild()
+    {
+        cout << "PublicChild constructor called" << endl;
+        publicVar = 12;
+        protectedVar = 12;
+        // privateVar = 12;
+        cout << publicVar << protectedVar << endl;
+    }
+};
+
+int main()
+{
+    PublicChild p1;
+    PrivateChild p2;
+    ProtectedChild p3;
 }
