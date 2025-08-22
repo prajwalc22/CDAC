576c036c96a7fe95aafb226e7f0b9766bfbd8005
prajwal22 <prajwalchoudhari22@gmail.com>
2025-08-22 20:49:08 +0530

    added sumofdigit

---
+++ codeforces/sumofdig.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        
+        int num;
+        cin >> num;
+        int sum = 0;
+        int temp = num / 10;
+        while (num > 0)
+        {                    
+            sum += num % 10; 
+            num = num / 10;  
+        }
+
+        cout << sum << endl;
+    }
+}
